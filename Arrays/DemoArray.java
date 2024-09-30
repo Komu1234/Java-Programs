2e7f77e40a13fa294cb8d1e2fc6361632159de7e
unknown <Komu1234>
2024-09-30 12:57:41 +0530

    My fourth commit

---
+++ Arrays/DemoArray.java
@@ -0,0 +1,13 @@
+package Arrays;
+public class DemoArray {
+  public static void main(String[] args) {
+      int[] anArray = new int[10];
+      for(int i = 0; i<anArray.length;i++){
+        int y = i +20*30;
+        System.out.println(anArray[i] = y); 
+        
+
+      }
+  }
+  
+}
